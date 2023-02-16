38d30264632d2bf72645f4353180315519715eda
Sukanya <106397577+Gund008@users.noreply.github.com>
2023-02-16 14:17:34 +0530

    Add files via upload

---
+++ StringObject.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Program
+{
+    public String str;
+    public Program(String s1)
+    {
+        this.str=s1;
+    }
+}
+
+public class StringX 
+{
+    public static void main(String arg[])
+    {
+        //String Literals
+        String s1="Hello";
+        String s2="Hello";
+
+        //String Reference
+        String s3=s1;
+
+        //String Object
+        String s4=new String("Hello");
+        String s5=new String("Hello");
+     
+    } 
+}
