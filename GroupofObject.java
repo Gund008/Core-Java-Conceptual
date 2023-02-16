e8bb8a63e678057ebf2d00abe2dab4dc45a92da6
Sukanya <106397577+Gund008@users.noreply.github.com>
2023-02-16 12:50:10 +0530

    Add files via upload

---
+++ GroupofObject.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+class Employee
+{
+    public int id;
+    public String name;
+
+    public Employee(int i,String n)
+    {
+        id=i;
+        name=n;
+    }
+    public void displaydata()
+    {
+        System.out.println(id+"\t"+name);
+    }
+}
+public class GroupofObject 
+{
+    public static void main(String arg[])
+    {
+        Scanner sobj=new Scanner(System.in);
+
+        System.out.println("Enter id");
+        int ino=sobj.nextInt();
+
+        System.out.println("Enter name");
+        String str=sobj.nextLine();
+
+        Employee Arr[]=new Employee[5];
+
+        for(int i=0;i<Arr.length;i++)
+        {
+            Arr[i]=new Employee(ino,str);  //call Constructor    
+        }
+        System.out.println("\nThe Employee data from array is");
+
+        for(int i=0;i<Arr.length;i++)
+        {
+            Arr[i].displaydata();
+        }
+    }
+}
